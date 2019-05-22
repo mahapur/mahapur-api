3786f38d771dbbf7878437ce2f4c7e2c1e898c06
Swapnil Gaikwad <swapnil.gaikwad@pubmatic.com>
2019-05-22 09:29:07 +0530

    Converted status code to 200 Ok

--- src/main/java/com/example/controller/mapper/ExceptionMapper.java
+++ src/main/java/com/example/controller/mapper/ExceptionMapper.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ExceptionMapper {
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
     public ErrorObject handleInternalError(Exception e) {
         return new ErrorObject("SL_001_001", e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
     public ErrorObject handleBadRequest(IllegalArgumentException e) {
         return new ErrorObject("SL_001_002", e.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
     public ErrorObject handleBadState(IllegalArgumentException e) {
         return new ErrorObject("SL_001_003", e.getMessage());
