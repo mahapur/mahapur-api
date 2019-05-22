2492d53880cda13f78ca57b2cc9aa501b6ff7433
Swapnil Gaikwad <swapnil.gaikwad@pubmatic.com>
2019-05-22 10:13:06 +0530

    Add error getter methods

--- src/main/java/com/example/controller/mapper/ErrorObject.java
+++ src/main/java/com/example/controller/mapper/ErrorObject.java
@@ -14,4 +14,20 @@ class ErrorObject {
 
     public ErrorObject() {
     }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
