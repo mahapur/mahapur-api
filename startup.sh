8ed28d0ee32544e2d7934d741af5bfac472287bb
Swapnil Gaikwad <swapnil.gaikwad@pubmatic.com>
2019-05-22 10:14:46 +0530

    Move docker files to docker folder

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-java -jar -Dspring.profiles.active=prod  -Dserver.port=${PORT} -XX:MaxRAM=100m -Xss512k App.jar
+echo '*****************************************************************'
+echo ${ENV}
+java -jar -Dspring.profiles.active=${ENV}  -Dserver.port=${PORT} -XX:MaxRAM=100m -Xss512k App.jar
